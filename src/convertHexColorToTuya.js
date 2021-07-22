595e5d499ab59453f8b292300956e1391737f355
BrOrlandi <brorlandi@gmail.com>
2021-07-22 02:10:36 -0300

    Fix setting light color with Math.round to hsv values.

--- src/convertHexColorToTuya.js
+++ src/convertHexColorToTuya.js
@@ -4,9 +4,9 @@ const { MAX_HUE, MAX_SATURATION, MAX_VALUE } = require('./constants')
 module.exports = function convertHexColorToTuya(hexColor) {
   const color = tinycolor(hexColor)
   const { h, s, v } = color.toHsv()
-  const hue = ((h / 360) * MAX_HUE).toString(16).padStart(4, '0')
-  const saturation = (s * MAX_SATURATION).toString(16).padStart(4, '0')
-  const value = (v * MAX_VALUE).toString(16).padStart(4, '0')
+  const hue = (Math.round((h / 360) * MAX_HUE)).toString(16).padStart(4, '0')
+  const saturation = (Math.round(s * MAX_SATURATION)).toString(16).padStart(4, '0')
+  const value = (Math.round(v * MAX_VALUE)).toString(16).padStart(4, '0')
 
   return hue + saturation + value
 }
