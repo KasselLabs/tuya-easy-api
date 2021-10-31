f36b29581f72491774562c579d930f3d3cd66af8
BrOrlandi <brorlandi@gmail.com>
2021-10-31 15:16:21 -0300

    Add Smartcurtain device support.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Light = require('./src/Light')
 const SmartPlug = require('./src/SmartPlug')
+const SmartCurtain = require('./src/SmartCurtain')
 
 module.exports = {
   Light,
   SmartPlug,
+  SmartCurtain,
 }
