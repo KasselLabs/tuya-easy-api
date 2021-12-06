e8757314b4466f367afe75288f915df501edfb76
Nihey Takizawa <nihey.takizawa@gmail.com>
2021-12-06 14:50:36 -0300

    :arrow_up: Make some adjustments o package.json and before 0.0.0 release

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const Device = require('./src/Device')
 const Light = require('./src/Light')
 const SmartPlug = require('./src/SmartPlug')
 const SmartCurtain = require('./src/SmartCurtain')
+const SmartSwitch = require('./src/SmartSwitch')
 
 module.exports = {
+  Device,
   Light,
   SmartPlug,
   SmartCurtain,
+  SmartSwitch,
 }
